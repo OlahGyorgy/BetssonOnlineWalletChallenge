64d3468e65fe4031c3a5eeaef14812c0851f7d24
Oláh György <public.email.gyorgy@gmail.com>
2024-10-15 07:40:16 +0200

    init APITest Framework

---
+++ src/Betsson.OnlineWallets.APITests/Logging/RestLogger.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+using Serilog;
+
+namespace Betsson.OnlineWallets.APITests.Logging;
+
+public class RestLogger
+{
+        public static void APILogger(RestRequest request,RestResponse response)
+        {
+                Log.Logger = new LoggerConfiguration()
+                        .WriteTo.Console()
+                        .CreateLogger();
+                
+                Log.Information("Sending request to  {Url} ", request.Resource);
+                Log.Information("Response: {StatusCode}, {ResponseContent}", response.StatusCode, response.Content);
+        }
+}
